0699019fc24c9fe70f8c8ec08f0935c27a77b4b0
Bruno Maciel <101736685+brunosmaciel@users.noreply.github.com>
2023-03-22 18:41:07 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,31 +11,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-        
-"primary": "#e8e114",
-        
-"secondary": "#4a66ad",
-        
-"accent": "#ed6dbc",
-        
-"neutral": "#1A1A29",
-        
-"base-100": "#34313A",
-        
-"info": "#6A96E7",
-        
-"success": "#52EBBF",
-        
-"warning": "#C57411",
-        
-"error": "#F54332",
-        },
-      },
-    ],
-  },
+  plugins: [],
+  
 }
